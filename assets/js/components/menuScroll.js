93135de461b91d10aa72fc2bda74a492506fb9e6
MarcelGFranca <marcelgarciafranca@gmail.com>
2019-05-21 00:59:09 -0300

    darken animation menu

--- assets/js/components/menuScroll.js
+++ assets/js/components/menuScroll.js
@@ -2,12 +2,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     console.log(document.documentElement.scrollTop);
-  if (document.body.scrollTop > 80 ||
-    document.documentElement.scrollTop > 80) {
+  if (document.body.scrollTop > 10 ||
+    document.documentElement.scrollTop > 10) {
     document.getElementById("menu").style.background = "#29234b";
     document.getElementById("menu").style.height = "80px";
+    menu.className="s-header--darken s-header";
   } else {
     document.getElementById("menu").style.background = "none";
     document.getElementById("menu").style.height = "90px";
+    menu.className="s-header s-header--undarken";
   }
 }
