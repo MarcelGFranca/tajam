c4eb00de9c229776c7d22e01c99b9a9121e07df7
MarcelGFranca <marcelgarciafranca@gmail.com>
2019-05-21 09:02:49 -0300

    header menu animation

--- assets/js/components/menuScroll.js
+++ assets/js/components/menuScroll.js
@@ -4,12 +4,10 @@ function scrollFunction() {
     console.log(document.documentElement.scrollTop);
   if (document.body.scrollTop > 10 ||
     document.documentElement.scrollTop > 10) {
-    document.getElementById("menu").style.background = "#29234b";
-    document.getElementById("menu").style.height = "80px";
-    menu.className="s-header--darken s-header";
+    // document.getElementById("menu").style.paddingTop = "10px";
+    menu.className="s-header s-header--darken";
   } else {
-    document.getElementById("menu").style.background = "none";
-    document.getElementById("menu").style.height = "90px";
-    menu.className="s-header s-header--undarken";
+    menu.className="s-header";
+    // document.getElementById("menu").style.paddingTop = "20px";
   }
 }
