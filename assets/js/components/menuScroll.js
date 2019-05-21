73c92bfa81619e3aa832b69186b9762360219afb
Marcel <marcel.franca@novahaus.com.br>
2019-05-21 12:18:44 -0300

    added hover to nav menu

--- assets/js/components/menuScroll.js
+++ assets/js/components/menuScroll.js
@@ -1,13 +1,10 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    console.log(document.documentElement.scrollTop);
   if (document.body.scrollTop > 10 ||
     document.documentElement.scrollTop > 10) {
-    // document.getElementById("menu").style.paddingTop = "10px";
     menu.className="s-header s-header--darken";
   } else {
     menu.className="s-header";
-    // document.getElementById("menu").style.paddingTop = "20px";
   }
 }
